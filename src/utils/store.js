2021651eb870a49a3b58483766f7859cf24ec679
Neha-singh18 <singhrajputneha896@gmail.com>
2025-12-17 13:48:53 +0530

    Fix: reset Redux on logout to prevent stale data; expand CORS to Vercel origins

--- src/utils/store.js
+++ src/utils/store.js
@@ -1,4 +1,4 @@
-import { configureStore } from "@reduxjs/toolkit";
+import { configureStore, combineReducers } from "@reduxjs/toolkit";
 import menuSlice from "./menuSlice";
 import userSlice from "./userSlice";
 import budgetReducer from "./budgetSlice";
@@ -7,16 +7,25 @@ import expenseReducer from "./expenseSlice";
 import dashboardReducer from "./dashboardSlice";
 import incomeReducer from "./incomeSlice";
 
-const store = configureStore({
-  reducer: {
-    menu: menuSlice,
-    user: userSlice,
-    expense: expenseReducer,
-    budget: budgetReducer,
-    category: categoryReducer,
-    dashboard: dashboardReducer,
-    income: incomeReducer,
-  },
+// Combine reducers first
+const appReducer = combineReducers({
+  menu: menuSlice,
+  user: userSlice,
+  expense: expenseReducer,
+  budget: budgetReducer,
+  category: categoryReducer,
+  dashboard: dashboardReducer,
+  income: incomeReducer,
 });
+
+// Root reducer that resets the whole store when user logs out
+const rootReducer = (state, action) => {
+  if (action.type === "user/removeUser") {
+    state = undefined; // let all slices return to their initial state
+  }
+  return appReducer(state, action);
+};
+
+const store = configureStore({ reducer: rootReducer });
 
 export default store;
